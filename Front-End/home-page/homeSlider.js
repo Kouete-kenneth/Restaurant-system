843a38dcb3911d7f1085f3a6cdf9d3dc9f00aac1
kouete kenneth <kouete678@gmail.com>
2023-07-20 09:32:01 +0100

    Restaurant website: temporary Front-End
    time taken 24 hours
    
    still under production

---
+++ Front-End/home-page/homeSlider.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const slider = document.querySelector(".slider");
+    const images = slider.querySelectorAll("img");
+    const sliderWidth = slider.offsetWidth;
+    let slideIndex = 0;
+  
+    function moveSlider() {
+      slider.style.transform = `translateX(-${slideIndex * sliderWidth}px)`;
+    }
+  
+    function nextSlide() {
+      slideIndex = (slideIndex + 1) % images.length;
+      moveSlider();
+    }
+  
+    function prevSlide() {
+      slideIndex = (slideIndex - 1 + images.length) % images.length;
+      moveSlider();
+    }
+  
+    document.getElementById("prevBtn").addEventListener("click", prevSlide);
+    document.getElementById("nextBtn").addEventListener("click", nextSlide);
+  
+    setInterval(nextSlide, 5000); // Change slide every 5 seconds
+  });
